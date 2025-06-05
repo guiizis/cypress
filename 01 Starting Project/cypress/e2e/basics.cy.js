1228d2f37b545b970c7a4a41b2143e753e7fbb80
guiziis <guigamoda@hotmail.com>
2025-06-05 20:18:49 -0300

    get with find

--- 01 Starting Project/cypress/e2e/basics.cy.js
+++ 01 Starting Project/cypress/e2e/basics.cy.js
@@ -8,6 +8,7 @@ describe('template spec', () => {
 
   it('should render the main page', () => {
     cy.get('.main-header img').should('be.visible');
+    cy.get('.main-header').find('img').should('be.visible');
     cy.get('[data-cy="main-header-logo"]').should('be.visible');
   });
 
