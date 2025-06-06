33f462b7dba402b74880dc1169423dde3ab1db2b
guiziis <guigamoda@hotmail.com>
2025-06-06 20:57:54 -0300

    tests main screen with backdrop visible or not

--- 01 Starting Project/cypress/e2e/tasks.cy.js
+++ 01 Starting Project/cypress/e2e/tasks.cy.js
@@ -4,12 +4,14 @@ describe('tasks management', () => {
   it('should open and close the new task modal', () => {
     cy.visit('http://localhost:5173/');
 
-    // Open the modal
     cy.get('[data-cy="start-add-task-button"]').click();
     cy.get('#new-task-form').should('be.visible');
 
-    // Close the modal
     cy.contains('Cancel').click();
     cy.get('#new-task-form').should('not.exist');
+
+    cy.get('[data-cy="start-add-task-button"]').click();
+    cy.get('.backdrop').click({force: true});
+    cy.get('.backdrop').should('not.exist');
   });
 });
