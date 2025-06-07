f201ac428819967be32be4ddb776ebda8f2c4ca8
guiziis <guigamoda@hotmail.com>
2025-06-06 21:20:29 -0300

    tests add tasks flow

--- 01 Starting Project/cypress/e2e/tasks.cy.js
+++ 01 Starting Project/cypress/e2e/tasks.cy.js
@@ -1,9 +1,11 @@
 /// <reference types="Cypress" />
 
 describe('tasks management', () => {
-  it('should open and close the new task modal', () => {
+  beforeEach(() => {
     cy.visit('http://localhost:5173/');
+  });
 
+  it('should open and close the new task modal', () => {
     cy.get('[data-cy="start-add-task-button"]').click();
     cy.get('#new-task-form').should('be.visible');
 
@@ -11,7 +13,32 @@ describe('tasks management', () => {
     cy.get('#new-task-form').should('not.exist');
 
     cy.get('[data-cy="start-add-task-button"]').click();
-    cy.get('.backdrop').click({force: true});
+    cy.get('.backdrop').click({ force: true });
     cy.get('.backdrop').should('not.exist');
+  });
+
+  it('should create a new task', () => {
+    const newTask = {
+      title: 'New Task',
+      summary: 'This is a new task summary',
+      category: 'low'
+    };
+
+    cy.get('[data-cy="start-add-task-button"]').click();
+
+    cy.get('#title').type(newTask.title);
+    cy.get('#summary').type(newTask.summary);
+    cy.get('#category').select(newTask.category);
+
+    cy.get('[type="submit"]').click();
+
+    cy.get('.task-list')
+      .find('.task')
+      .first()
+      .should((li) => {
+        expect(li).to.have.descendants('span');
+        expect(li).to.have.descendants('h2').to.contain(newTask.title);
+        expect(li).to.have.descendants('p').to.contain(newTask.summary);
+      });
   });
 });
