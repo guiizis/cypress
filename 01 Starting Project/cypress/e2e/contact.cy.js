fd56a61344f165acf65aba5197403e6646086bb4
guiziis <guigamoda@hotmail.com>
2025-06-08 12:26:34 -0300

    add validation to form

--- 01 Starting Project/cypress/e2e/contact.cy.js
+++ 01 Starting Project/cypress/e2e/contact.cy.js
@@ -37,4 +37,55 @@ describe('contact form', () => {
     cy.get('#name').type('John Doe');
     cy.get('#email').type('test@email.com{enter}');
   });
+
+  it('should validate the form by button', () => {
+    cy.get('[data-cy="contact-btn-submit"]').as('btnSubmit');
+
+    cy.get('@btnSubmit').then(($btn) => {
+      cy.wrap($btn).click();
+
+      expect($btn)
+        .to
+        .be
+        .not
+        .disabled;
+
+      expect($btn)
+        .to
+        .have
+        .text('Send Message');
+    })
+  });
+
+  it('should validate the form by form fields', () => {
+    cy.get('[data-cy="contact-btn-submit"]').as('btnSubmit');
+    const formFields = [
+      {
+        id: 'contact-input-message',
+        validContent: 'This is a test message'
+      },
+      {
+        id: 'contact-input-name',
+        validContent: 'John Doe'
+      },
+      {
+        id: 'contact-input-email',
+        validContent: 'content@email.com'
+      }
+    ];
+
+    formFields.forEach((field) => {
+      cy.get(`[data-cy="${field.id}"]`).click();
+    });
+
+    cy.get('@btnSubmit').click();
+    cy.get('form').find('[class*="_invalid_"]').should('have.length', formFields.length);
+
+
+    formFields.forEach((field, i) => {
+      cy.get(`[data-cy="${field.id}"]`).type(field.validContent);
+      cy.get('@btnSubmit').click();
+      cy.get('form').find('[class*="_invalid_"]').should('have.length', formFields.length - (i + 1));
+    });
+  });
 });
