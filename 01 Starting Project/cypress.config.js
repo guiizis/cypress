106191c5b3cadcb56a511b991d904af2f7fcd053
guiziis <guigamoda@hotmail.com>
2025-06-08 15:05:52 -0300

    add baseUrl config

--- 01 Starting Project/cypress.config.js
+++ 01 Starting Project/cypress.config.js
@@ -3,7 +3,8 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   video: false,
   e2e: {
-    setupNodeEvents(on, config) {
+    baseUrl: "http://localhost:5173",
+    c(on, config) {
       // implement node event listeners here
     },
   },
