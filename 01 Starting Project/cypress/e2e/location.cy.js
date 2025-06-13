70bdf75749962cfb5d3a5bfbc188cd7eb5a0587f
guiziis <guigamoda@hotmail.com>
2025-06-13 20:46:35 -0300

    empty stub

--- 01 Starting Project/cypress/e2e/location.cy.js
+++ 01 Starting Project/cypress/e2e/location.cy.js
@@ -10,5 +10,8 @@ describe('share location', () => {
   it('should fetch the user location', () => {
     cy.get('[data-cy="get-loc-btn"]').click();
     cy.get('@getCurrentPosition').should('be.called');
+
+    cy.get('[data-cy="get-loc-btn"]').should('be.disabled');
+    cy.get('[data-cy="actions"]').should('contain', 'location fetched');
   });
 });
