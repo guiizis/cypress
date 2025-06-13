56b412e652e2458be4c2ebb252e9fdaf80892a53
guiziis <guigamoda@hotmail.com>
2025-06-12 21:14:08 -0300

    change project base

--- 01 Starting Project/cypress.config.js
+++ 01 Starting Project/cypress.config.js
@@ -1,17 +1,10 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  video: false,
   e2e: {
-    baseUrl: "http://localhost:5173",
-
+    baseUrl: 'http://localhost:5173',
     setupNodeEvents(on, config) {
-      on('task', {
-        seedDatabase(isJustATestFileName) {
-          console.log('Seeding database...' + isJustATestFileName);
-          return null; // Return null or any value you want
-        }
-      });
+      // implement node event listeners here
     },
   },
 });
--- 01 Starting Project/cypress/support/commands.js
+++ 01 Starting Project/cypress/support/commands.js
@@ -23,15 +23,3 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-
-Cypress.Commands.add('submitForm', () => {
-  cy.get('form button[type="submit"]').click();
-});
-
-Cypress.Commands.addQuery('getByTestId', (testId) => {
-  const getFn = cy.now('get', `[data-cy="${testId}"]`);
-  return () => {
-    const element = getFn();
-    return element;
-  }
-});
