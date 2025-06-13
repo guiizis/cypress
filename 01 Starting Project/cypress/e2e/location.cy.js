167c85191a72b896960ba2095227bea8b405e588
guiziis <guigamoda@hotmail.com>
2025-06-12 21:28:49 -0300

    add our first stub

--- 01 Starting Project/cypress/e2e/location.cy.js
+++ 01 Starting Project/cypress/e2e/location.cy.js
@@ -1,8 +1,14 @@
 /// <reference types="cypress" />
 
 describe('share location', () => {
+  beforeEach(() => {
+    cy.visit('/').then(window => {
+      cy.stub(window.navigator.geolocation, 'getCurrentPosition').as('getCurrentPosition');
+    });
+  });
+
   it('should fetch the user location', () => {
-    cy.visit('/');
     cy.get('[data-cy="get-loc-btn"]').click();
+    cy.get('@getCurrentPosition').should('be.called');
   });
 });
