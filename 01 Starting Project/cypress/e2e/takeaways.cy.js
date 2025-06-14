667180b29790554e1d5069b2fa659f61a9712395
guiziis <guigamoda@hotmail.com>
2025-06-14 19:28:22 -0300

    add task to fake DB

--- 01 Starting Project/cypress/e2e/takeaways.cy.js
+++ 01 Starting Project/cypress/e2e/takeaways.cy.js
@@ -3,7 +3,7 @@
 describe('Takeaways', () => {
   beforeEach(() => {
     cy.visit('/');
-    cy.task('task'); // Seed the database with initial data
+    cy.task('seedDatabase'); // Seed the database with initial data
   });
 
   it('should display a list of fetched takeaways', () => {
