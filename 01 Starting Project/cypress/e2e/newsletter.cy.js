3fa2061a4f82cb619dbaed7620300035d5eb851a
guiziis <guigamoda@hotmail.com>
2025-06-14 19:56:37 -0300

    adds interceptor to error

--- 01 Starting Project/cypress/e2e/newsletter.cy.js
+++ 01 Starting Project/cypress/e2e/newsletter.cy.js
@@ -1,21 +1,39 @@
 /// <reference types="Cypress" />
 
 describe('Newsletter', () => {
-  beforeEach(() => {
+  beforeEach(() => { });
+
+  it('should display a success message', () => {
     cy.intercept('POST', '/newsletter*', {
       status: 201,
       body: {
         message: 'Thanks for singing up',
       },
     }).as('newsletterSubmit');
-    cy.visit('/');
-    cy.task('seedDatabase'); // Seed the database with initial data
-  });
 
-  it('should display a success message', () => {
+    cy.visit('/');
+    cy.task('seedDatabase');
+
     cy.get('[data-cy="newsletter-email"]').type('test@test.com');
     cy.get('[data-cy="newsletter-submit"]').click();
     cy.wait('@newsletterSubmit');
     cy.contains('Thanks for singing up');
+  });
+
+  it('should display an error message for invalid email', () => {
+    cy.visit('/');
+    cy.task('seedDatabase');
+
+    cy.intercept('POST', '/newsletter*', {
+      status: 400,
+      body: {
+        message: 'Email already exists',
+      },
+    }).as('newsletterSubmitError');
+
+    cy.get('[data-cy="newsletter-email"]').type('test@test');
+    cy.get('[data-cy="newsletter-submit"]').click();
+    cy.wait('@newsletterSubmitError');
+    cy.contains('Email already exists');
   });
 });
